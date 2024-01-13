7fd1268cddca72575f506c0f83fb7a22a7b01aa9
César O. Araújo <oaraujocesar@gmail.com>
2024-01-13 17:37:41 -0300

    format stuff

--- src/layouts/public.layout.tsx
+++ src/layouts/public.layout.tsx
@@ -4,7 +4,7 @@ import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 export const PublicLayout = (): JSX.Element => {
   return (
     <main className="flex">
-        {/* TODO: Add public header */}
+      {/* TODO: Add public header */}
       <aside className="bg-black text-white w-40 flex justify-center items-center">
         <h1>Public</h1>
       </aside>
