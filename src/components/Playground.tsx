ede67ac6654f38824c15fd9b927e1dfb9170a46e
Pedro Silva <silvasantospedro91@gmail.com>
2024-01-14 19:34:58 -0300

    Test bun format pre-commit

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -7,12 +7,12 @@ import { TrashIcon } from '@heroicons/react/24/solid'
 export const Playground = () => {
   return (
     <div className="w-[300px] py-8 px-4 rounded-lg bg-white flex flex-col">
-      <Label htmlFor="email" label="Email" />
+                         <Label htmlFor="email" label="Email" />
       <Input type="email" id="email" placeholder="Email" />
       <Label htmlFor="password" label="Password" />
-      <Input type="password" id="password" placeholder="••••••••••" />
+                <Input type="password" id="password" placeholder="••••••••••" />
       <Button variant={'default'} size={'sm'}>
-        <TrashIcon className='w-4 h-4'/>
+                  <TrashIcon className="w-4 h-4" />
         Delete
       </Button>
     </div>
