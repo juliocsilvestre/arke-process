df9658611028c20eb6f6a36c8a763a3e499942eb
Julio Silvestre <jcss.silvestre@gmail.com>
2024-01-14 16:32:59 -0300

    fix formatting

--- src/layouts/dashboard.layout.tsx
+++ src/layouts/dashboard.layout.tsx
@@ -1,14 +1,14 @@
-import { Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const DashboardLayout = () => {
-    return (
-        <div className="flex">
-            <aside className="bg-black text-white w-40 flex justify-center items-center">
-                <h1>Dashboard</h1>
-            </aside>
-            <Outlet />
-            <TanStackRouterDevtools position="bottom-right" />
-        </div>
-    );
-};
+  return (
+    <div className="flex">
+      <aside className="bg-black text-white w-40 flex justify-center items-center">
+        <h1>Dashboard</h1>
+      </aside>
+      <Outlet />
+      <TanStackRouterDevtools position="bottom-right" />
+    </div>
+  )
+}
--- src/layouts/public.layout.tsx
+++ src/layouts/public.layout.tsx
@@ -1,17 +1,17 @@
-import { Footer } from "@/components/footer/Footer";
-import { Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { Footer } from '@/components/footer/Footer'
+import { Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const PublicLayout = (): JSX.Element => {
-    return (
-        <main className="flex">
-            {/* TODO: Add public header */}
-            <aside className="bg-black text-white w-40 flex justify-center items-center">
-                <h1>Public</h1>
-            </aside>
-            <Outlet />
-            <TanStackRouterDevtools position="bottom-right" />
-            <Footer />
-        </main>
-    );
-};
+  return (
+    <main className="flex">
+      {/* TODO: Add public header */}
+      <aside className="bg-black text-white w-40 flex justify-center items-center">
+        <h1>Public</h1>
+      </aside>
+      <Outlet />
+      <TanStackRouterDevtools position="bottom-right" />
+      <Footer />
+    </main>
+  )
+}
