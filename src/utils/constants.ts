a2541cd6a4dad0d57aa44952352df5f6ec986178
César O. Araújo <oaraujocesar@gmail.com>
2024-01-18 17:29:30 -0300

    Update navigation and redirect in SignIn page

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
+import { BuildingOfficeIcon, ChartBarIcon, ShieldCheckIcon, TicketIcon, UserGroupIcon } from '@heroicons/react/24/solid'
+
 export const SIZE = {
   xs: 'xs',
   sm: 'sm',
@@ -5,3 +7,59 @@ export const SIZE = {
   lg: 'lg',
   xl: 'xl',
 } as const
+
+export const CPF_REGEXP = /^(\d{3}.?\d{3}.?\d{3}-?\d{2})$/
+export const CNPJ_REGEXP = /^(\d{2}.?\d{3}.?\d{3}\/?\d{4}-?\d{2})$/
+export const PHONE_REGEXP = /^(\(\d{2}\) \d{5}-\d{4})$/
+export const CEP_REGEXP = /^(\d{5}-?\d{3})$/
+
+export const UF_LIST = {
+  AC: 'AC',
+  AL: 'AL',
+  AP: 'AP',
+  AM: 'AM',
+  BA: 'BA',
+  CE: 'CE',
+  DF: 'DF',
+  ES: 'ES',
+  GO: 'GO',
+  MA: 'MA',
+  MT: 'MT',
+  MS: 'MS',
+  MG: 'MG',
+  PA: 'PA',
+  PB: 'PB',
+  PR: 'PR',
+  PE: 'PE',
+  PI: 'PI',
+  RJ: 'RJ',
+  RN: 'RN',
+  RS: 'RS',
+  RO: 'RO',
+  RR: 'RR',
+  SC: 'SC',
+  SP: 'SP',
+  SE: 'SE',
+  TO: 'TO',
+} as const
+
+export type UF = (typeof UF_LIST)[keyof typeof UF_LIST]
+
+export const NAVIGATION = [
+  { name: 'Administradores', href: '/dashboard/administradores', icon: ShieldCheckIcon },
+  { name: 'Funcionários', href: '/dashboard/funcionarios', icon: UserGroupIcon },
+  { name: 'Fornecedores', href: '/dashboard/fornecedores', icon: BuildingOfficeIcon },
+  { name: 'Eventos', href: '/dashboard/eventos', icon: TicketIcon },
+  { name: 'Relatórios', href: '/dashboard/relatorios', icon: ChartBarIcon },
+]
+
+export const WORKER_STATUS = {
+  active: 'active',
+  expelled: 'expelled',
+  banished: 'banished',
+} as const
+
+export type WorkerStatus = (typeof WORKER_STATUS)[keyof typeof WORKER_STATUS]
+
+export const MAX_FILE_SIZE = 1024 * 1024 * 2 // 2MB
+export const ACCEPTED_IMAGE_TYPES = ['image/png', 'image/jpeg', 'image/jpg', 'image/gif']
