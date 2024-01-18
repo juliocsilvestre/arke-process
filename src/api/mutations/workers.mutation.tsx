0dc0a8bc5cd59a2e2da2740fd8e0472aea2458ec
César O. Araújo <oaraujocesar@gmail.com>
2024-01-18 17:48:05 -0300

    create workers mutation

---
+++ src/api/mutations/workers.mutation.tsx
@@ -0,0 +1,25 @@
+import { CreateWorkerBody } from '@/pages/Workers.defs'
+import { useMutation } from '@tanstack/react-query'
+import { api } from '../api'
+
+export const useCreateWorker = () => {
+  const mutation = useMutation({
+    mutationFn: (worker: CreateWorkerBody) => {
+      console.log(worker.picture)
+
+      const formData = new FormData()
+
+      for (const [key, value] of Object.entries(worker)) {
+        formData.append(key, value)
+      }
+
+      return api.post('/workers', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+    },
+  })
+
+  return { ...mutation }
+}
