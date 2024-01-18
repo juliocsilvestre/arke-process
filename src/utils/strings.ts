39086dc9ef8133ef1570618ad8c136d528c97b71
César O. Araújo <oaraujocesar@gmail.com>
2024-01-18 17:29:09 -0300

    Add string masking functions for phone numbers and CEPs

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -16,3 +16,19 @@ export const maskCNPJ = (cnpj: string) => {
     .replace(/(\d{4})(\d{1,2})/, '$1-$2')
     .replace(/(-\d{2})\d+?$/, '$1')
 }
+
+export const maskPhoneNumber = (phone: string) => {
+  return phone
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{4})(\d)/, '$1-$2')
+    .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+    .replace(/(-\d{4})\d+?$/, '$1')
+}
+
+export const maskCEP = (cep: string) => {
+  return cep
+    .replace(/\D/g, '')
+    .replace(/(\d{5})(\d)/, '$1-$2')
+    .replace(/(-\d{3})\d+?$/, '$1')
+}
