30b212b462393f56969267ec9986ce6e86933d45
Pedro Silva <silvasantospedro91@gmail.com>
2024-01-23 22:13:14 -0300

    Refactor events.query.tsx and Events.page.tsx

--- src/api/queries/events.query.tsx
+++ src/api/queries/events.query.tsx
@@ -1,15 +1,29 @@
 import { queryOptions, useQuery } from '@tanstack/react-query'
 import { api } from '../api'
+import { Pagination } from '@/utils/types'
 
-export const getEvents = async () => await api.get('/events')
-export const indexEventsQueryOption = queryOptions({
-  queryKey: ['events'],
-  queryFn: getEvents,
+export const getEvents = async (pagination?: Pagination) => {
+  // Construct the base path with optional query parameters
+  const path = '/events'
+  const queryParams = new URLSearchParams({
+    q: pagination?.q || '', // Use empty string if q is not provided
+    page: pagination?.page || '1', // Use empty string if page is not provided
+  })
+
+  // Combine the base path with query parameters
+  const url = `${path}?${queryParams.toString()}`
+
+  // Perform the API request
+  return await api.get(url)
+}
+
+export const indexEventsQueryOption = (pagination?: Pagination) => queryOptions({
+  queryKey: ['events', pagination],
+  queryFn: () => getEvents(pagination),
 })
-export const useIndexEvents = () => {
-  const events = useQuery(indexEventsQueryOption)
 
-  return { events }
+export const useIndexEvents = (pagination?: Pagination) => {
+  return useQuery(indexEventsQueryOption(pagination))
 }
 
 export const getSingleEvent = async (id: string) => {
