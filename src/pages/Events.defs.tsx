af81fff3f8db012e13dec188688f371c8b01b3a4
Julio Silvestre <jcss.silvestre@gmail.com>
2024-01-25 21:45:20 -0300

    delete button for event

--- src/pages/Events.defs.tsx
+++ src/pages/Events.defs.tsx
@@ -1,92 +1,137 @@
-import { ColumnDef } from '@tanstack/react-table'
-import { z } from 'zod'
-import { formatDate } from '@utils/constants'
+import { ColumnDef } from "@tanstack/react-table";
+import { Tooltip } from "react-tooltip";
+import { z } from "zod";
+
+import { formatDate } from "@utils/constants";
+import { Button } from "@/components/ui/Button";
+import { TrashIcon } from "@heroicons/react/24/solid";
+import { AxiosError } from "axios";
+import { toast } from "sonner";
+import { ConfirmationModal } from "@/components/ConfirmationModal";
+import { useDeleteEvent } from "@/api/mutations/events.mutation";
 
 export const CreateEventSchema = z.object({
-  name: z
-    .string()
-    .min(2, { message: 'Nome deve conter pelo menos 2 caracteres.' })
-    .max(50, { message: 'Nome deve conter no máximo 50 caracteres.' }),
-  dates: z.object({
-    from: z.date(),
-    to: z.date(),
-  }),
-})
+    name: z
+        .string()
+        .min(2, { message: "Nome deve conter pelo menos 2 caracteres." })
+        .max(50, { message: "Nome deve conter no máximo 50 caracteres." }),
+    dates: z.object({
+        from: z.date(),
+        to: z.date(),
+    }),
+});
 
-export type CreateEventBody = z.infer<typeof CreateEventSchema>
+export type CreateEventBody = z.infer<typeof CreateEventSchema>;
 
 export type EventDay = {
-  id: string
-  event_id: string
-  date: Date
-  created_at: Date
-  updated_at: Date
-}
+    id: string;
+    event_id: string;
+    date: Date;
+    created_at: Date;
+    updated_at: Date;
+};
 
 export interface Event {
-  id: string
-  name: string
-  admin_id: string
-  start_date: Date
-  finish_date: Date
-  days: EventDay[]
-  created_at: Date
-  updated_at: Date
-  deleted_at: Date
+    id: string;
+    name: string;
+    admin_id: string;
+    start_date: Date;
+    finish_date: Date;
+    days: EventDay[];
+    created_at: Date;
+    updated_at: Date;
+    deleted_at: Date;
 }
 
+const _DeleteEventButton = ({ event }: { event: Event }) => {
+    const { mutateAsync: deleteCompany } = useDeleteEvent();
+
+    const onDeleteCompany = async (event: Event): Promise<void> => {
+        try {
+            await deleteCompany(event.id);
+            toast.success(<p>O fornecedor "{event.name}" foi excluído com sucesso!</p>);
+        } catch (error: unknown) {
+            if (!(error instanceof AxiosError)) return;
+            console.error(error.response?.data.message);
+        }
+    };
+
+    return (
+        <ConfirmationModal
+            title={
+                <span>
+                    Você tem certeza de que deseja apagar <strong>"{event.name}"</strong>?
+                </span>
+            }
+            description="Esta ação não pode ser desfeita. Isso excluirá permanentemente o fornecedor."
+            variant={"destructive"}
+            actionButtonLabel="Apagar"
+            onAction={() => void onDeleteCompany(event)}
+        >
+            <Button
+                data-tooltip-id={`delete-company-${event.id}`}
+                data-tooltip-content={`Apagar "${event.name}"`}
+                variant="destructive"
+                size="icon"
+            >
+                <TrashIcon className="w-4 h-4" />
+            </Button>
+            <Tooltip id={`delete-company-${event.id}`} place="top" />
+        </ConfirmationModal>
+    );
+};
+
 export const eventsColumns: ColumnDef<Event>[] = [
-  {
-    accessorKey: 'name',
-    header: 'Nome',
-  },
-  {
-    accessorKey: 'created_at',
-    header: 'Criado em',
-    cell: ({ row }) => {
-      const date = new Date(row.getValue('created_at'))
-      return formatDate(date)
+    {
+        accessorKey: "name",
+        header: "Nome",
     },
-  },
-  {
-    accessorKey: 'first_day',
-    header: 'Data de início',
-    cell: ({ row }) => {
-      const event = row.original
+    {
+        accessorKey: "created_at",
+        header: "Criado em",
+        cell: ({ row }) => {
+            const date = new Date(row.getValue("created_at"));
+            return formatDate(date);
+        },
+    },
+    {
+        accessorKey: "first_day",
+        header: "Data de início",
+        cell: ({ row }) => {
+            const event = row.original;
 
-      const date = new Date(event?.start_date ?? null)
-      return formatDate(date)
+            const date = new Date(event?.start_date ?? null);
+            return formatDate(date);
+        },
     },
-  },
-  {
-    accessorKey: 'finish_day',
-    header: 'Data de fim',
-    cell: ({ row }) => {
-      const event = row.original
-      const date = new Date(event?.finish_date ?? null)
-      return formatDate(date)
+    {
+        accessorKey: "finish_day",
+        header: "Data de fim",
+        cell: ({ row }) => {
+            const event = row.original;
+            const date = new Date(event?.finish_date ?? null);
+            return formatDate(date);
+        },
     },
-  },
-  {
-    accessorKey: 'updated_at',
-    header: 'Atualizado em',
-    cell: ({ row }) => {
-      const date = new Date(row.getValue('updated_at'))
-      return formatDate(date)
+    {
+        accessorKey: "updated_at",
+        header: "Atualizado em",
+        cell: ({ row }) => {
+            const date = new Date(row.getValue("updated_at"));
+            return formatDate(date);
+        },
     },
-  },
-  // TODO: add actions
-  // {
-  //   header: 'Ações',
-  //   id: 'actions',
-  //   cell: ({ row }) => {
-  //     const company = row.original
+    {
+        header: "Ações",
+        id: "actions",
+        cell: ({ row }) => {
+            const event = row.original;
 
-  //     return (
-  //       <div className="flex justify-start">
-  //         <_DeleteCompanyButton company={company} />
-  //       </div>
-  //     )
-  //   },
-  // },
-]
+            return (
+                <div className="flex justify-start">
+                    <_DeleteEventButton event={event} />
+                </div>
+            );
+        },
+    },
+];
