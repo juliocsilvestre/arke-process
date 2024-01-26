ff450ba16c3c8aac8ec12d5f369436a5aa67a386
Caio Fernando <caiofsr13@gmail.com>
2024-01-26 20:43:27 -0300

    fine adjusts on bracelet

--- src/components/ui/Bracelet.pdf.tsx
+++ src/components/ui/Bracelet.pdf.tsx
@@ -3,7 +3,7 @@ import { Document, Font, Image, Page, StyleSheet, Text, View } from '@react-pdf/
 
 // Create styles
 const styles = StyleSheet.create({
-  page: { backgroundColor: 'transparent', display: 'flex', alignItems: 'center', justifyContent: 'space-between' },
+  page: { backgroundColor: 'transparent', display: 'flex', alignItems: 'flex-start', paddingLeft: 80 },
   section: {
     fontSize: 18,
     marginTop: 5,
@@ -25,12 +25,11 @@ export const BraceletPDF = ({ qrcode, worker }: { qrcode: string; worker: Worker
           <Image style={{ width: 69, height: 69 }} src={`data:image/png;base64, ${qrcode}`} />
         </View>
         <View style={styles.section}>
-          <Text style={{ fontFamily: 'Helvetica', fontWeight: 'black', marginTop: 10 }}>
+          <Text style={{ fontFamily: 'Helvetica', fontWeight: 'black', marginTop: 5 }}>
             {worker.full_name.toUpperCase()}
           </Text>
-          <Text style={{ fontSize: 14 }}>
-            {worker.role} - {worker.company?.name}
-          </Text>
+          <Text style={{ fontSize: 14 }}>{worker.role}</Text>
+          <Text style={{ fontSize: 14 }}>{worker.company?.name}</Text>
         </View>
       </View>
     </Page>
