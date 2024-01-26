df4346d5cffe35a9e78268a1ad9c07d7193f56b1
Julio Silvestre <jcss.silvestre@gmail.com>
2024-01-25 21:45:40 -0300

    bun format

--- src/api/mutations/events.mutation.tsx
+++ src/api/mutations/events.mutation.tsx
@@ -1,67 +1,67 @@
-import { CreateEventBody } from "@/pages/Events.defs";
-import { queryClient } from "@/routes";
-import { useMutation } from "@tanstack/react-query";
-import { eachDayOfInterval } from "date-fns";
-import { api } from "../api";
-import { indexEventsQueryOption } from "../queries/events.query";
+import { CreateEventBody } from '@/pages/Events.defs'
+import { queryClient } from '@/routes'
+import { useMutation } from '@tanstack/react-query'
+import { eachDayOfInterval } from 'date-fns'
+import { api } from '../api'
+import { indexEventsQueryOption } from '../queries/events.query'
 
 export const useCreateEvent = () => {
-    const mutation = useMutation({
-        mutationFn: (event: CreateEventBody) => {
-            const arrayOfDates = eachDayOfInterval({
-                start: event.dates.from,
-                end: event.dates.to,
-            });
+  const mutation = useMutation({
+    mutationFn: (event: CreateEventBody) => {
+      const arrayOfDates = eachDayOfInterval({
+        start: event.dates.from,
+        end: event.dates.to,
+      })
 
-            return api.post("/events", { ...event, dates: arrayOfDates });
-        },
-        onSuccess: () => {
-            queryClient.invalidateQueries(indexEventsQueryOption({ page: "1", q: "" }));
-        },
-    });
+      return api.post('/events', { ...event, dates: arrayOfDates })
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(indexEventsQueryOption({ page: '1', q: '' }))
+    },
+  })
 
-    return { ...mutation };
-};
+  return { ...mutation }
+}
 
 export const useAttachWorkerToAnEventDay = () => {
-    const mutation = useMutation({
-        mutationFn: (assignement: {
-            eventId: string;
-            workers_id: string[];
-            event_day_id: string;
-        }) => {
-            return api.post(`/events/${assignement.eventId}/attach`, {
-                workers_id: assignement.workers_id,
-                event_day_id: assignement.event_day_id,
-            });
-        },
-    });
+  const mutation = useMutation({
+    mutationFn: (assignement: {
+      eventId: string
+      workers_id: string[]
+      event_day_id: string
+    }) => {
+      return api.post(`/events/${assignement.eventId}/attach`, {
+        workers_id: assignement.workers_id,
+        event_day_id: assignement.event_day_id,
+      })
+    },
+  })
 
-    return { ...mutation };
-};
+  return { ...mutation }
+}
 
 export const useClockWorkerOnEventDay = () => {
-    const mutation = useMutation({
-        mutationFn: (assignement: { event_day_id: string; worker_id: string }) => {
-            return api.post("/workers/clocks", {
-                event_day_id: assignement.event_day_id,
-                worker_id: assignement.worker_id,
-            });
-        },
-    });
+  const mutation = useMutation({
+    mutationFn: (assignement: { event_day_id: string; worker_id: string }) => {
+      return api.post('/workers/clocks', {
+        event_day_id: assignement.event_day_id,
+        worker_id: assignement.worker_id,
+      })
+    },
+  })
 
-    return { ...mutation };
-};
+  return { ...mutation }
+}
 
 export const useDeleteEvent = () => {
-    const mutation = useMutation({
-        mutationFn: (id: string) => {
-            return api.delete(`/events/${id}`);
-        },
-        onSuccess: () => {
-            queryClient.invalidateQueries(indexEventsQueryOption({ page: "1", q: "" }));
-        },
-    });
+  const mutation = useMutation({
+    mutationFn: (id: string) => {
+      return api.delete(`/events/${id}`)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(indexEventsQueryOption({ page: '1', q: '' }))
+    },
+  })
 
-    return { ...mutation };
-};
+  return { ...mutation }
+}
