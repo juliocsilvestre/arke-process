b13930626ee478cea58a9aa5a89d28d8a7392d4e
César O. Araújo <oaraujocesar@gmail.com>
2024-02-05 22:17:26 -0300

    fix image deployment

--- src/pages/SignIn.page.tsx
+++ src/pages/SignIn.page.tsx
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import * as z from 'zod'
 
-// import { zodResolver } from '@hookform/resolvers/zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { useNavigate } from '@tanstack/react-router'
 
 import { Button } from '@components/ui/Button'
@@ -14,14 +14,13 @@ import { useSignInMutation } from '@/api/mutations/auth.mutation'
 import { useAuthStore } from '@/store/auth.store'
 import { checkError } from '@/utils/errors'
 import { maskCPF } from '@/utils/strings'
-import { useEffect } from 'react'
 import Logo from '../assets/carvalogo.svg'
 import { SignInSchema, SigninBodyKeys } from './SignIn.defs'
 import { type SignInBody } from './SignIn.defs'
 
 export const SignIn = () => {
   const form = useForm<z.infer<typeof SignInSchema>>({
-    // resolver: zodResolver(SignInSchema),
+    resolver: zodResolver(SignInSchema),
     defaultValues: {
       cpf: '',
       password: '',
@@ -53,9 +52,6 @@ export const SignIn = () => {
     }
   }
 
-  useEffect(() => {
-    console.error(form.formState.errors)
-  }, [form.formState.errors])
   return (
     <div className="w-[400px] mx-4 md:mx-0 py-[45px] px-8 rounded-lg bg-white flex flex-col">
       <Form {...form}>
